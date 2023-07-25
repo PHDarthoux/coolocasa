214074b4dadef37cf16c422b39cf110ec46b3625
PHDarthoux <darthoux.pierre.henri@gmail.com>
2023-07-25 16:12:02 +0200

    fix: logout + feat: flash message connexion

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -18,13 +18,18 @@ class LoginController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        $this->addFlash(
+            'success',
+            'Connexion réussie!'
+        );
+
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $error,
         ]);
     }
 
-    #[Route('/logout', name: 'app_logout', methods: ['POST'])]
+    #[Route('/logout', name: 'app_logout')]
     public function logout(): never
     {
         // controller can be blank: it will never be called!
